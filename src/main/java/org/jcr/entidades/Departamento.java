4b4b9d4be9aa5fa827f5fc4d947de6a353fdb9a7
Matias <82124436+Gonzalez-Matias@users.noreply.github.com>
2025-09-29 21:33:34 -0300

    Proyecto añadido

---
+++ src/main/java/org/jcr/entidades/Departamento.java
@@ -0,0 +1,68 @@
+package org.jcr.entidades;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@ToString(exclude = {"medicos", "salas"})
+public class Departamento implements Serializable {
+    private final String nombre;
+    private final EspecialidadMedica especialidad;
+    private Hospital hospital;
+    private final List<Medico> medicos = new ArrayList<>();
+    private final List<Sala> salas = new ArrayList<>();
+
+    @Builder
+    public Departamento(String nombre, EspecialidadMedica especialidad) {
+        this.nombre = validarString(nombre);
+        this.especialidad = Objects.requireNonNull(especialidad, "La especialidad no puede ser nula");
+    }
+
+    public void setHospital(Hospital hospital) {
+        if (this.hospital != hospital) {
+            if (this.hospital != null) {
+                this.hospital.getInternalDepartamentos().remove(this);
+            }
+            this.hospital = hospital;
+            if (hospital != null) {
+                hospital.getInternalDepartamentos().add(this);
+            }
+        }
+    }
+
+    public void agregarMedico(Medico medico) {
+        if (medico != null && !medicos.contains(medico)) {
+            medicos.add(medico);
+            medico.setDepartamento(this);
+        }
+    }
+
+    public Sala crearSala(String numero, String tipo) {
+        Sala sala = new Sala(numero, tipo, this);
+        salas.add(sala);
+        return sala;
+    }
+
+    public List<Medico> getMedicos() {
+        return Collections.unmodifiableList(medicos);
+    }
+
+    public List<Sala> getSalas() {
+        return Collections.unmodifiableList(salas);
+    }
+
+
+    private String validarString(String valor) {
+        Objects.requireNonNull(valor, "El nombre del departamento no puede ser nulo ni vacío");
+        if (valor.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre del departamento no puede ser nulo ni vacío");
+        }
+        return valor;
+    }
+}
